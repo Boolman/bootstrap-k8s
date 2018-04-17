3de2e7682261840a9bd45643039bfdbb519be774
Emil Öhgren <boolman@gmail.com>
2018-04-17 15:43:41 +0200

    update script to update ports in os

---
+++ update-os-ports.sh
@@ -0,0 +1,10 @@
+#!/bin/ash
+set -e
+netone=$(cat group_vars/k8s-cluster.yaml | grep kube_service_addresses | awk '{print $2}')
+nettwo=$(cat group_vars/k8s-cluster.yaml | grep ^kube_pods_subnet | awk '{print $2}')
+while read host
+do
+  ip=$(openstack server list -f value -c Networks | grep ${host} | grep -E -o "\w+=[^ ,]+" | cut -d "=" -f2)
+  openstack port list -c ID -c 'Fixed IP Addresses' -f value | grep ${ip} | awk '{print $1}' | xargs -r -IPORTID echo openstack port set --allowed-address ${netone} --allowed-address ${nettwo} PORTID
+done < <(cat inventory | grep ansible_ssh_host | cut -d "=" -f2 | sort -u)
+
