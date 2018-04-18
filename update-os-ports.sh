1b10612c804ca02ffd61eccb5087efd3a2d88493
Emil Öhgren <boolman@gmail.com>
2018-04-18 11:51:15 +0200

    change syntax

--- update-os-ports.sh
+++ update-os-ports.sh
@@ -5,5 +5,5 @@ nettwo=$(cat group_vars/k8s-cluster.yaml | grep ^kube_pods_subnet | awk '{print 
 while IFS= read -r host
 do
   ip=$(openstack server list -f value -c Networks | grep ${host} | grep -E -o "\w+=[^ ,]+" | cut -d "=" -f2)
-  openstack port list -c ID -c 'Fixed IP Addresses' -f value | grep ${ip} | awk '{print $1}' | xargs -r -IPORTID echo openstack port set --allowed-address ${netone} --allowed-address ${nettwo} PORTID
+  openstack port list -c ID -c 'Fixed IP Addresses' -f value | grep ${ip} | awk '{print $1}' | xargs -r -IPORTID echo openstack port set --allowed-address ip-address=${netone} --allowed-address ip-address=${nettwo} PORTID
 done
